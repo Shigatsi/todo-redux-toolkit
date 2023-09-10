cc490deab89be5c7d431244ef4c6e8eda4d58ee8
shigatsi <shigatsii@gmail.com>
2023-09-10 23:09:45 +0300

    setup gh-pages

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# todo-redux-toolkit
+### todo-redux-toolkit
 Проект создан для ознакомления с Redux Toolkit.
 
 Технологии: React, Redux Toolkit, TypeScript, AntDesign.
+
+#### Заголовок h4
+для запуска локально в корне проекта выполнить команды:
+1. npm install
+2. npm start
--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Divider } from "antd";
+import { Col, Divider, Row } from "antd";
 import Item from "../Item/Item.tsx";
 import { RootState } from "../../store/store.tsx";
 
 const ItemList = () => {
   const { todos } = useSelector((state: RootState) => state.todo);
   return (
-    <div className="item-list">
+    <div className="item-list" style={{ margin: "0 auto" }}>
       <Divider orientation="left">ToDo:</Divider>
       {todos.map((el) => (
-        <Item key={el.id} {...el} />
+            <Item key={el.id} {...el} />
+
+
       ))}
     </div>
   );
